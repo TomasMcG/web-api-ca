285b15c3e1ac8dcb44f3188b2d9aefb3233410fd
tomas <tomasmcgrath21@gmail.com>
2025-12-21 16:38:28 +0000

    Added basic review Model api for user reviews

---
+++ movies-api/api/reviews/reviewModel.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+  movieId: { type:Number, required: true },
+  rating: { type: Number, enum: [1,2,3,4,5], required: true },
+  created_at: {type:Date, default: Date.now},
+  content: { type: String, required: true },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+},
+authorName: {type:String, required: true},
+
+});
+
+
+export default mongoose.model('Review', ReviewSchema);
